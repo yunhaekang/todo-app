4291f42feddbf5913238cbd3427fa8bc5ed49a35
yunhaekang <yunhaekang1@gmail.com>
2023-09-12 13:36:14 +0900

    api 서버 호스트 URI의 프로토콜을 https 로 변경

--- todo-app-ui/src/api-config.js
+++ todo-app-ui/src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
     backendHost = "http://localhost:8080";
 } else {
-    backendHost = "http://prod-todo-api-service-yunoi.ap-northeast-2.elasticbeanstalk.com";
+    backendHost = "https://api.yunhaekang.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
