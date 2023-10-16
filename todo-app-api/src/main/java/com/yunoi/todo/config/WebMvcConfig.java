10c63148a4d0af77cf41ee82dd564939156c24e1
yunhaekang <yunhaekang1@gmail.com>
2023-10-16 17:38:58 +0900

    설명을 위한 주석 추가

--- todo-app-api/src/main/java/com/yunoi/todo/config/WebMvcConfig.java
+++ todo-app-api/src/main/java/com/yunoi/todo/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("http://localhost:3000",
-                        "https://app.yunhaekang.com")
+                        "https://app.yunhaekang.com")   // Origin(출처): Protocol + Host + Port
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 메서드 작성
                 .allowedHeaders("*")    // 모든 헤더 정보 허용
                 .allowCredentials(true) // 인증에 관한 정보 허용
